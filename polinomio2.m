b100698181ab16b5ceb176f2fa46e1171fa95cb2
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2021-11-17 13:15:47 -0600

    Update and rename radical4.m to polinomio2.m

--- polinomio2.m
+++ polinomio2.m
@@ -1,6 +1,6 @@
 % Octave Script
 %School                :Tecnologico de Estudios Superiores de Jilotepec 
-%Specialty             :Ingeniería en Sistemas Computacionales 
+%Specialty             :IngenierÃ­a en Sistemas Computacionales 
 % Title                :Funcion polinominal o racional
 % Description          :Script de ejercicios polinominal 
 % Author               :fernando angel
@@ -14,4 +14,4 @@ x=[-10:1:10]
 fx=((2.*x.^2)+(x.^4)+(x))
 plot(x,fx)
 grid on;
-title("Radical");
+title("polinomio f(x)=(-0.4533976515164,0");
